340da35b35fffb9689f73a2eefcc747c6fcf1006
Arslanbaig84 <arslan.multiline@gmail.com>
2024-10-17 17:15:50 +0500

    static filed loaded now

--- b2bsolutions/settings.py
+++ b2bsolutions/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+#STATIC_ROOT = BASE_DIR / 'staticfiles'  # Only used in production
