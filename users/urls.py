0d833304b6b803acaf650abddb8932bae776396f
Arslanbaig84 <arslan.multiline@gmail.com>
2024-10-18 15:31:56 +0500

    login and logout routes

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('register', views.register, name='register'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('login_user', views.login_user, name='login_user'),
+    path('logout_user', views.logout_user, name='logout_user')
 ]
