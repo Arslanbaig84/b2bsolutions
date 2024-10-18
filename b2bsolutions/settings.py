6a7352bca2f47265be1ca3fef8e17eb63a0e75a8
Arslanbaig84 <arslan.multiline@gmail.com>
2024-10-18 07:30:07 +0500

    nothing changed

--- b2bsolutions/settings.py
+++ b2bsolutions/settings.py
@@ -114,15 +114,13 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CustomUser to be treated as default user model
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -130,3 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 #STATIC_ROOT = BASE_DIR / 'staticfiles'  # Only used in production
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+#STATIC_URL = 'static/'
