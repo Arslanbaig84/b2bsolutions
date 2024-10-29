0a682db7e145798e3b6acfce63dd827482990fce
Arslanbaig84 <arslan.multiline@gmail.com>
2024-10-29 13:02:12 +0500

    widgets for update profile form

--- users/forms.py
+++ users/forms.py
@@ -17,10 +17,26 @@ class UserProfileForm(forms.ModelForm):
     
     industry_type = forms.ModelMultipleChoiceField(
             queryset = Industry.objects.all(),
-            widget= forms.CheckboxSelectMultiple,
+            widget= forms.CheckboxSelectMultiple(
+                attrs={'class':'border-0'}
+            ),
             required = True
         )
 
     class Meta:
         model = UserProfile
         fields = ['business_name', 'organization_type', 'business_type', 'no_of_employees', 'industry_type', 'ntn', 'contact', 'whatsapp', 'website', 'address1', 'address2', 'establishment_year']
+
+        widgets = {
+            'business_name':forms.TextInput(attrs={'class':'border-0 col-12', 'autofocus':'true'}),
+            'organization_type':forms.Select(attrs={'class':'border-0 col-12'}),
+            'business_type':forms.Select(attrs={'class':'border-0 col-12'}),
+            'no_of_employees':forms.Select(attrs={'class':'border-0 col-12'}),
+            'ntn':forms.TextInput(attrs={'class':'border-0 col-12'}),
+            'contact':forms.TextInput(attrs={'class':'border-0 col-12'}),
+            'whatsapp':forms.TextInput(attrs={'class':'border-0 col-12'}),
+            'website':forms.URLInput(attrs={'class':'border-0 col-12'}),
+            'address1':forms.TextInput(attrs={'class':'border-0 col-12'}),
+            'address2':forms.TextInput(attrs={'class':'border-0 col-12'}),
+            'establishment_year':forms.NumberInput(attrs={'class':'border-0 col-12'}),
+        }
